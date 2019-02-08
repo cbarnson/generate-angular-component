3f6e4ce8a6f8554c1711a95b4776a381aa67cf24
Cody Barnson <cody.barnson@arcurve.com>
2019-02-07 17:19:42 -0700

    Update, added spec

--- gen.py
+++ gen.py
@@ -24,7 +24,25 @@ import re
 import os
 import textwrap
 import sys
+import string
 
+def ng_component_spec_template(base_kebab, classname):
+    return textwrap.dedent("""\
+        import { %s } from "./%s.component";
+
+        describe("%s", () => {
+          let component: %s;
+          beforeEach(() => {
+            component = new %s();
+          });
+
+          describe("foo", () => {
+            it("foo is foo", () => {
+              expect(component).toBeDefined();
+            });
+          });
+        });
+        """ % (classname, base_kebab, classname, classname, classname))
 
 def ng_component_html_template():
     return textwrap.dedent("""\
@@ -105,6 +123,11 @@ class NgKebabConverter(KebabCaseConverter):
         result = self.convert_to_kebab(input_string)
         return result + ".component.ts"
 
+    # E.g. DocViewer --> doc-viewer.component.spec.ts
+    def spec_file_name(self, input_string):
+        result = self.convert_to_kebab(input_string)
+        return result + ".component.spec.ts"
+
 
 def test():
     converter = KebabCaseConverter(False)
@@ -119,12 +142,15 @@ def component_class_name(input_string):
     return input_string if (str(input_string).endswith("Component")) else input_string + "Component"
 
 
-def generate(converter, line):
+def generate(converter, line, safe_mode = False):
     # Auto detect a different "mode", where input is just case
     # insensitive but words are delimited by spaces; build the string to
     # CamelCase style, then proceed as normal
+    dprint(f"Inside generate : line = {line}, safe_mode = {safe_mode}")
     if (line.find(" ") != -1):
         line = "".join(map(lambda x: x.capitalize(), line.split(" ")))
+        # line = string.capwords(line, "")
+        dprint(f"capwords : {line}")
     assert(line.find(" ") is -1)
 
     print("")
@@ -143,20 +169,36 @@ def generate(converter, line):
     selector = converter.selector(line)
     template_filename = converter.template_file_name(line)
     component_filename = converter.component_file_name(line)
+    spec_filename = converter.spec_file_name(line)
 
     try:
+        # ========================================================
+        # BEGIN : OUTPUT LIST OF COMPUTED VALUES
+        # ========================================================
+
         # Setup, print computed values
         destination = os.path.join(os.getcwd(), base_kebab)
         template = os.path.join(destination, template_filename)
         component = os.path.join(destination, component_filename)
+        spec = os.path.join(destination, spec_filename)
         index = os.path.join(destination, "index.ts")
         print("> Targets")
         print("-" * 40)
         print("Destination  : %s" % (destination))
         print("Template     : %s" % (template))
         print("Component    : %s" % (component))
+        print("Spec         : %s" % (spec))
         print("Index        : %s" % (index))
         print("")
+
+        # For dry-runs, i.e. "safe_mode", stop here
+        if (safe_mode is True):
+            print("Exiting... (dry-run only)")
+            return
+
+        # ========================================================
+        # BEGIN : CREATE FILES
+        # ========================================================
 
         # Create the destination directory
         assert(not os.path.exists(destination))
@@ -175,24 +217,35 @@ def generate(converter, line):
         with open(component, "w") as text_file:
             print(f"{content_ts}", file=text_file)
 
+        # Create .spec.ts
+        content_spec = ng_component_spec_template(
+            base_kebab, classname)
+        with open(spec, "w") as text_file:
+            print(f"{content_spec}", file=text_file)
+
         # Create .index.ts
         content_index_ts = ng_component_index_template(base_kebab)
         with open(index, "w") as text_file:
             print(f"{content_index_ts}", file=text_file)
+
+        # ========================================================
+        # BEGIN : APPEND/MODIFY FILES
+        # ========================================================
 
         # Update parent index
         parent_cwd = os.path.realpath(os.path.join(destination, ".."))
         parent_index = os.path.join(parent_cwd, "index.ts")
         print("parent_cwd : %s" % (parent_cwd))
         print("parent_index : %s" % (parent_index))
-        index_line = textwrap.dedent("""export * from "./%s/%s.component";
-        """ % (base_kebab, base_kebab))
+        index_line = textwrap.dedent("""\
+            export * from "./%s/%s.component";""" % (base_kebab, base_kebab))
         with open(parent_index, "a") as text_file:
             print(f"{index_line}", file=text_file)
 
         # Checks
         assert(os.path.exists(os.path.join(destination, component_filename)))
         assert(os.path.exists(os.path.join(destination, template_filename)))
+        assert(os.path.exists(os.path.join(destination, spec_filename)))
         print("Files successfully created.")
         print("All done!")
         print("")
@@ -202,30 +255,65 @@ def generate(converter, line):
         exit(2)
 
 
+def preamble():
+    print("")
+    print("=================== Angular Component Generator ===================")
+    print("Usage: gen.cmd [--dry-run] [optional space delimited list of words]")
+    print("===================================================================")
+    print("")
+
+# ========================================
+# toggle DEBUG print statements here!!!
+# ========================================
+def dprint(s):
+    DEBUG_MODE = False
+    if (DEBUG_MODE):
+        print(s)
+
 def main():
     converter = NgKebabConverter()
+    safe_mode = False
 
     # If run with args, use the arg list as a single line of input
     # E.g. gen.py hello world
     if (len(sys.argv) > 1):
-        args = " ".join(sys.argv[1:]).strip()
-        print("line : %s" % (args))
-        assert(len(args) > 0)
-        try:
-            generate(converter, args)
-        except Exception:
-            exit(3)
-        return
+        raw_args = sys.argv[1:]
+        dprint(f"sys.argv : {raw_args}")
 
+        if ("--help" in raw_args):
+            raw_args.remove("--help")
+            dprint(f"After --help option removed: {raw_args}")
+            preamble()
+
+        if ("--dry-run" in raw_args):
+            raw_args.remove("--dry-run")
+            dprint(f"After --dry-run option removed: {raw_args}")
+            safe_mode = True
+
+
+        # Post-option removal, if still have args, run once with them, else move to the general loop below
+        dprint(f"len(raw_args) : {len(raw_args)}")
+        if (len(raw_args) > 0):
+            # Space-separated list of the command line arguments, after options are removed
+            args = " ".join(raw_args)
+            dprint(f"line : {args}")
+            try:
+                dprint(f"safe_mode : {safe_mode}")
+                generate(converter, args, safe_mode)
+            except Exception:
+                exit(3)
+            return
+
+    # For invocation without command line arguments (that are NOT options)
     while True:
         try:
-            line = input().strip()
-            print("line : %s" % (line))
+            line = input("New component (e.g. my new item, MyNewItem)\nPlease enter name (empty RETURN to quit):").strip()
             if (line is ""):
                 print("Exiting...")
                 break
 
-            generate(converter, line)
+            print("line : %s" % (line))
+            generate(converter, line, safe_mode)
 
         except EOFError:
             print("ERROR: Must exit...")
